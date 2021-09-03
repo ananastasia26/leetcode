120597bdb73b9461db6067576779fc34d4e762a2
a-leonova <nastyaleo98@yandex.ru>
2021-09-03 16:51:13 +0700

    Remove useless lines

--- twosums/main.py
+++ twosums/main.py
@@ -1,7 +1,6 @@
 def twoSum(nums, target: int):
     diffs = {}
     for idx, num in enumerate(nums):
-        # TODO:  corner cases!
         pair = target - num
         if pair in diffs:
             pairIdx = diffs[pair]
@@ -9,8 +8,3 @@ def twoSum(nums, target: int):
         else:
             diffs[num] = idx
     return [-1, -1]
-    
-res = twoSum([11, 2, 15, 7], target = 9)
-res2 = twoSum([4, 2, 4, 2], target = 8)
-print(res)
-print(res2)
